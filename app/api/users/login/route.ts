f7c61acc88244c4cc72e50c81dbe57ed39a4b51a
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-09-21 07:19:19 -0400

    Login backend w dependencies

---
+++ app/api/users/login/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+        
+        const reqBody = await request.json()
+        const {email, password} = reqBody;
+        console.log(reqBody);
+
+        //Check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+
+        //Check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if(!validPassword){
+            return NextResponse.json({error: "Invalid password"}, {status: 400})
+        }
+
+        // Create Token Data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        //Create token
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, {expiresIn: "1d"})
+
+        const response = NextResponse.json({
+            message: "Login is Successful",
+            success: true,
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        })
+        return response; 
+
+
+
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
