a00244b6c65687ffabb64163c5ffe9e02068b6c9
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-09-22 16:18:28 -0400

    Created Dashboard page

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -13,26 +13,22 @@ export async function POST(request: NextRequest) {
         const {email, password} = reqBody;
         console.log(reqBody);
 
-        //Check if user exists
         const user = await User.findOne({email})
         if(!user){
             return NextResponse.json({error: "User does not exist"}, {status: 400})
         }
 
-        //Check if password is correct
         const validPassword = await bcryptjs.compare(password, user.password)
         if(!validPassword){
             return NextResponse.json({error: "Invalid password"}, {status: 400})
         }
 
-        // Create Token Data
         const tokenData = {
             id: user._id,
             username: user.username,
             email: user.email
         }
 
-        //Create token
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, {expiresIn: "1d"})
 
         const response = NextResponse.json({
@@ -43,8 +39,6 @@ export async function POST(request: NextRequest) {
             httpOnly: true,
         })
         return response; 
-
-
 
 
     } catch (error: any) {
