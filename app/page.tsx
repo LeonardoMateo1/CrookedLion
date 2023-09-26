72de1c12ecb8cacdc3188b4ba8611aeefe6aee7e
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-09-26 00:38:58 -0400

    Get Started Responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       <Empower/>
       <Blog/>
       <Testimonials/>
-      {/* <GetStarted/>
-      <Footer/> */}
+      <GetStarted/>
+      {/* <Footer/> */}
     </main>
   )
 }
