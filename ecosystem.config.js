8f79f12ba3cbd49f7ce1b7073d58dffae7c8547c
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-10-01 13:11:29 -0400

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  }],
+
+  deploy : {
+    production : {
+      key: 'crookedlion.pem',
+      user : 'ubuntu',
+      host : '54.152.95.176',
+      ref  : 'origin/main',
+      repo : 'git@github.com:LeonardoMateo1/CrookedLion.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
